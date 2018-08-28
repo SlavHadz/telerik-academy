1d8a9dd92387d0c21453e7dd60ea8ba6bdb8d65e
SlavHadz <nufcfen@gmail.com>
2018-08-28 18:55:04 +0300

    Creating gallery data file and gallery tamplate

--- js-applications/spa project/data.js
+++ js-applications/spa project/data.js
@@ -1,7 +1,36 @@
 var data = {
-    a: 5,
-    b: 7,
-    c: 1
+    team: [
+        {
+            player: 'Georgi Kostadinov',
+            img: './imgs/georgiKostadinov.jpg'
+        },
+        {
+            player: 'Ivaylo Chochev',
+            img: './imgs/chocev.jpg'
+        },
+        {
+            player: 'Vasil Bozhikov',
+            img: './imgs/bozhikov.jpg'
+        },
+        {
+            player: 'Andrey Galabinov',
+            img: './imgs/galabinov.jpg'
+        },
+        {
+            player: 'Svetoslav Dyakov',
+            img: './imgs/dyakov.jpg'
+        },
+        {
+            player: 'Anton Nedyalkov',
+            img: './imgs/nedyalkov.jpg'
+        },
+        {
+            player: 'Plamen Iliev',
+            img: './imgs/plamenIliev.jpg'
+        },
+        {
+            player: 'Ivelin Popov',
+            img: './imgs/popov.jpg'
+        }
+    ]
 };
-
-export {data};
---
+++ js-applications/spa project/templates.js
@@ -0,0 +1,6 @@
+<div class="gallery">
+    {{#team}}
+    <h1>{{player}}</h1>
+    <img src="{{img}}"/>
+    {{/team}}
+</div>
